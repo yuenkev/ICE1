1969cce9f8f5611b846caf1c1693870a06bbee5b
yuenkev <yuenkev@sheridancollege.ca>
2022-02-04 14:26:05 -0500

    Committing "Kevin_Yuen_ICE1" Branch, Adding a object of Card Class in "CardDealer.java" file

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -7,15 +7,19 @@ package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
  * The class where we create Cards
- * @author dancye, 2019
+ * @author Kevin Yuen
+ * Student Number: 991652450
  */
 public class CardDealer 
 {
     public static void main(String[] args)
     {
-        //Hello Hello, this is for my ICE 1 In-Class Exercise
+        //1. Hello Hello, this is for my ICE 1 In-Class Exercise
         //My name is Kevin Yuen (Student Number: 991652450)
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        
+        //3. Creating an object of the Card Class
+        Card c1 = new Card("Diamond", 10);
     }
 }
